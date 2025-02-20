09daf0835c8eeda652328e8d65dc930f8893a228
guillermoCarrion <a27306@svalero.com>
2025-02-20 17:08:05 +0100

    base evento detalle

--- src/components/DetalleEventoComp.vue
+++ src/components/DetalleEventoComp.vue
@@ -4,11 +4,30 @@
 </script>
 
 <template>
-    <article class="evento">
-        <section class="evento__detalle">
-            
-            
-
-        </section>
-    </article>
+    <main class="main">
+        <article class="evento">
+            <h1 class="evento__titulo">TITULO DEL EVENTO</h1>
+            <section class="evento__detalle">
+                <img alt="Imagen Evento" class="evento__imagen">
+                <div class="evento__texto"></div>
+                    <h3 class="evento__subtitle">Descripcion</h3>
+                    <span>esta es la desc del evetno</span>
+            </section>
+        </article>
+    </main>
 </template>
+
+<style lang="scss">
+h1 {
+  margin-top: 5%;
+  border: #d40202;
+  background-color: rgba(0, 0, 0, 0.88);
+  font-size: 24px;
+  text-align: center;
+  margin-bottom: 30px;
+  color: #d40202;
+  text-shadow: 2px 2px 5px rgba(0, 0, 0, 0.7);
+  width: 100%;
+  padding: 10px;
+}
+</style>
